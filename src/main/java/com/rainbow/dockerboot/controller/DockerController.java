be4f64cb9e04e3fd78871d5e0bb0169a583df5a0
JhonRain <jiangy19@126.com>
2019-11-28 08:36:52 +0800

    Update DockerController.java

--- src/main/java/com/rainbow/dockerboot/controller/DockerController.java
+++ src/main/java/com/rainbow/dockerboot/controller/DockerController.java
@@ -1,9 +1,15 @@
 package com.rainbow.dockerboot.controller;
 
+import com.rainbow.dockerboot.dao.CarDao;
+import com.rainbow.dockerboot.dao.UserDao;
+importcom.rainbow.dockerboot.entity.Car;
+import org.springframework.beans.factory.annotation.Autowired;
 import com.rainbow.dockerboot.dto.Response;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 /**
  * <p>功能描述</br></p>
@@ -21,4 +27,26 @@ public class DockerController {
     public Response info() {
         return Response.build("Hello Springboot Docker!");
     }
+    
+    @Autowired
+    private UserDao userDao;
+    @Autowired
+    private CarDao carDao;
+
+    @GetMapping(value = "/insert-mongodb")
+    public String insertMongoDB() {
+        Car car = new Car();
+        car.setId(new Random().nextInt(15000000));
+        String number = String.valueOf(System.currentTimeMillis());
+        car.setNumber(number);
+        carDao.save(car);
+        return "this is insert-mongodb";
+    }
+
+    @GetMapping(value = "/insert-mysql")
+    public String insertMySQL() {
+        userDao.insert();
+        return "this is insert-mysql";
+    }
+    
 }
