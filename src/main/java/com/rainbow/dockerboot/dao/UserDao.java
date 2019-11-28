70d5b626b155bfc09cbe3e2e37eb84c502ed3cd5
JhonRain <jiangy19@126.com>
2019-11-28 17:56:57 +0800

    Update UserDao.java

--- src/main/java/com/rainbow/dockerboot/dao/UserDao.java
+++ src/main/java/com/rainbow/dockerboot/dao/UserDao.java
@@ -8,22 +8,44 @@ import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
-
+/**
+ * <p>功能描述</br></p>
+ *
+ * @author rain
+ * @version v1.0
+ * @projectName docker-boot
+ * @date 2019/11/25 19:10
+ */
 @Repository
 public class UserDao {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-
+    /**
+     * <pre>插入用户</pre>
+     */
     public void insert() {
         String time = String.valueOf(System.currentTimeMillis());
-        String sql = "insert into t_user (username) values ('idea-" + time + "')";
+        String sql = "insert into t_user (username) values ('docker-" + time + "')";
         jdbcTemplate.update(sql);
         System.out.println("==========执行插入语句==========");
     }
 
+    /**
+     * <pre>查询用户列表</pre>
+     *
+     * @return
+     */
+    public List<User> queryUserList() {
+        return jdbcTemplate.query("select id,username from t_user", new UserMapper());
+    }
+
+    /**
+     * <pre>結果转换</pre>
+     */
     class UserMapper implements RowMapper<User> {
 
         @Override
