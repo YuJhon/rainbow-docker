498a3ee2e27567c9d7ba2654bc53a22c31a021f4
JhonRain <jiangy19@126.com>
2019-11-28 17:56:13 +0800

    Update User.java

--- src/main/java/com/rainbow/dockerboot/entity/User.java
+++ src/main/java/com/rainbow/dockerboot/entity/User.java
@@ -4,6 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * <p>功能描述</br></p>
+ *
+ * @author rain
+ * @version v1.0
+ * @projectName docker-boot
+ * @date 2019/11/25 19:10
+ */
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
